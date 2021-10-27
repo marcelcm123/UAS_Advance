1cd7d59d1e4d17b266daeae394ea7679ebd83d9a
marcelcm123 <liongmarcel@gmail.com>
2021-10-28 06:00:19 +0800

    update 3

--- app/src/main/java/ac/id/ubaya/todoapp/model/TodoDao.kt
+++ app/src/main/java/ac/id/ubaya/todoapp/model/TodoDao.kt
@@ -5,17 +5,17 @@ import androidx.room.*
 @Dao
 interface TodoDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAll(vararg todo:Todo)
+    suspend fun insertAll(vararg todo:Todo)
 
     @Query("SELECT * FROM todo")
-    fun selectAllTodo(): List<Todo>
+    suspend fun selectAllTodo(): List<Todo>
 
     @Query("SELECT * FROM todo WHERE uuid= :id")
-    fun selectTodo(id:Int): Todo
+    suspend fun selectTodo(id:Int): Todo
 
     @Query("UPDATE todo SET title=:title, notes=:notes, priority=:priority WHERE uuid = :id")
-    fun update(title:String, notes:String, priority:Int, id:Int)
+    suspend fun update(title:String, notes:String, priority:Int, id:Int)
 
     @Delete
-    fun deleteTodo(todo:Todo)
+    suspend fun deleteTodo(todo:Todo)
 }
--- app/src/main/java/ac/id/ubaya/todoapp/view/MainActivity.kt
+++ app/src/main/java/ac/id/ubaya/todoapp/view/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        navController = Navigation.findNavController(this,R.id.fragmentHost)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
     override fun onSupportNavigateUp(): Boolean {
